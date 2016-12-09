4eb3ab913873e5e2ced9c55796ca9bb518e11488
matth3wjones <matthewjones00@me.com>
2016-12-09 12:36:26 -0800

    Updated with test of code

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,29 +1,40 @@
 puts "Enter First Name"
 firstName = gets.chomp
-puts "Enter Middle Initial"
-middleInitial = gets.chomp
 puts "Enter Last Name"
 lastName = gets.chomp
 puts "Enter Age"
 applicantAge = gets.chomp
 puts "Enter Year Born"
-birthYear = gets.chomp
-puts "Would you like some Garlic Bread fresh from our cafeteria?"
+birthYear = gets.chomp.to_i
+puts "Would you like some garlic bread fresh from our cafeteria? Answer y/n"
 garlicReply = gets.chomp
-puts "Would you like to enroll in the health insurance plan offered by the company?"
+puts "Would you like to enroll in the health insurance plan? Answer y/n"
 insuranceReply = gets.chomp
-currentYear = Time.new.year
-birthYear - currentYear = ageCheck
-firstName + lastName = fullName
 
-if ageCheck == applicantAge && (garlicReply == true || insuranceReply == true)
-  puts "Probably not a vampire"
-elsif ageCheck != applicantAge && (garlicReply == false || insuranceReply == false)
-  puts "Probably a vampire"
-elsif ageCheck != applicantAge && (garlicReply ==false && insuranceReply == false)
-  puts "Almost certainly a vampire"
-elsif fullName == "Drake Cula" || fullName == "Tu Fang"
+currentYear = Time.new.year
+estimatedAge = currentYear - birthYear
+finalAge = true
+fullName = firstName + " " + lastName
+
+puts "Results for #{[firstName, lastName].compact.join(' ')}:"
+#Comparison of the Applicant's claimed age versus their estimated age
+puts "Estimated Age: #{estimatedAge}"
+puts "Claimed Age: #{applicantAge}"
+#Formula checks if the Applicant supplied the correct age, accounting for if applicant's birthday has not happened.
+if estimatedAge.to_i == applicantAge.to_i || (estimatedAge.to_i - 1) == applicantAge.to_i
+  finalAge = true
+else
+  finalAge = false
+end
+
+if fullName == "Drake Cula" || fullName == "Tu Fang"
   puts "Definitely a vampire"
+elsif finalAge == false && (garlicReply == "n" && insuranceReply == "n")
+  puts "Almost certainly a vampire"
+elsif finalAge == false && (garlicReply == "n" || insuranceReply == "n")
+  puts "Probably a vampire"
+elsif finalAge == true && (garlicReply == "y" || insuranceReply == "y")
+  puts "Probably not a vampire"
 else
   puts "Results inconclusive"
 end
