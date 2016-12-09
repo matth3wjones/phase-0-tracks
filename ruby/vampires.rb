faabe20d862a4af922e784be9732434b104a6ab2
matth3wjones <matthewjones00@me.com>
2016-12-09 13:39:06 -0800

    updated with release 4

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -11,14 +11,33 @@ for i in 0..(numApplicants-1)
   puts "Enter Year Born"
   birthYear = gets.chomp.to_i
   puts "Would you like some garlic bread fresh from our cafeteria?  Answer y/n"
-  garlicReply = gets.chomp
+  garlicReply = gets.chomp.downcase
   puts "Would you like to enroll in the health insurance plan? Answer y/n "
-  insuranceReply = gets.chomp
+  insuranceReply = gets.chomp.downcase
+
+allergyVamp = nil
+
+  loop do
+    puts "Do you have any allergies? When finished, or if no allergies type done"
+    allergyAnswer = gets.chomp.downcase
+    if allergyAnswer == "sunshine"
+      allergyVamp = true
+      break
+    elsif allergyAnswer == "done"
+      break
+    end
+  end
 
   currentYear = Time.new.year
   estimatedAge = currentYear - birthYear
-  finalAge = true
+  finalAge = nil
   fullName = firstName + " " + lastName
+
+  if estimatedAge.to_i == applicantAge.to_i || (estimatedAge.to_i - 1) ==  applicantAge.to_i
+    finalAge = true
+  else
+    finalAge = false
+  end
 
   puts "Results for #{[firstName, lastName].compact.join(' ')}:"
   #Comparison of the Applicant's claimed age versus their estimated age
@@ -26,22 +45,18 @@ for i in 0..(numApplicants-1)
   #puts "Claimed Age: #{applicantAge}"
 
   #Formula checks if the Applicant supplied the correct age, accounting   for if applicant's birthday has not happened.
-
-  if estimatedAge.to_i == applicantAge.to_i || (estimatedAge.to_i - 1) ==  applicantAge.to_i
-    finalAge = true
-  else
-    finalAge = false
-  end
-  #Formula to Review aswers
-  if fullName == "Drake Cula" || fullName == "Tu Fang"
+  if allergyVamp == true
     puts "Definitely a vampire"
-  elsif finalAge == false && (garlicReply == "n" && insuranceReply == "n" )
+  elsif fullName == "Drake Cula" || fullName == "Tu Fang"
+    puts "Definitely a vampire"
+  elsif finalAge == false && (garlicReply == "n" && insuranceReply == " n" )
     puts "Almost certainly a vampire"
-  elsif finalAge == false && (garlicReply == "n" || insuranceReply == "n" )
+  elsif finalAge == false && (garlicReply == "n" || insuranceReply == " n" )
     puts "Probably a vampire"
-  elsif finalAge == true && (garlicReply == "y" || insuranceReply == "y")
+  elsif finalAge == true && (garlicReply == "y" || insuranceReply == "y ")
     puts "Probably not a vampire"
   else
     puts "Results inconclusive"
   end
 end
+
