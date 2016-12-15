92fe45c30e622edab05294f3570c65f7005812ca
matth3wjones <matthewjones00@me.com>
2016-12-14 22:57:07 -0800

    add user input data to hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -19,14 +19,25 @@
 #
 #BUSINESS LOGIC
 #
-
+clientProfile = {
+  name: "",
+  age: "",
+  city: "",
+  sqFt: "",
+  kids: "",
+  theme: ""
+}
 
 #USER INTERFACE
 #
 puts "Enter client's full name"
 clientName = gets.chomp
+clientProfile[:name] = clientName
+#requests, collects, and adds name to hash
 puts "Enter client age"
 clientAge = gets.chomp.to_i
+clientProfile[:age] = clientAge
+#requests, collects, and adds age to hash
 puts "Enter client city"
 clientCity = gets.chomp
 puts "How many square feet is #{clientName}'s place?"
@@ -35,5 +46,7 @@ puts "How many children does #{clientName} have?"
 clientKids = gets.chomp.to_i
 puts "What is the decor/theme?"
 clientTheme = gets.chomp
+
+p clientProfile
 
 
