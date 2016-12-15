317182551c1fe5e5a37d3e311b5c2d65f7f0089d
matth3wjones <matthewjones00@me.com>
2016-12-14 22:24:31 -0800

    initial commit

---
+++ ruby/designer.rb
@@ -0,0 +1,14 @@
+applicationCompleted = {
+ name: "Matthew Jones",
+ address: "877 Island",
+ email: "matthewjones00@me.com",
+ phone: "619.208.4157",
+ faveShadeOfBlue: "cerulean",
+ wallpaperPreference: ["Paisley","Chevrons"],
+ ombre: "So last season"
+}
+
+applicationCompleted[:address] = "877 Island 312"
+p applicationCompleted
+applicationCompleted[:hired] = "No"
+p applicationCompleted
