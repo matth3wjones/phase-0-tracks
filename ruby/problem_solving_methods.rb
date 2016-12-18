a0300b4c5e28c2732daba5a573db0118e41d9599
matth3wjones <matthewjones00@me.com>
2016-12-18 13:00:24 -0800

    added sorting method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -50,3 +50,54 @@ else
 end
 
 #=>218922995834555169026
+
+#sorting method
+#
+#
+#create an array of unorganized items
+#use method to print out an organized version of the array
+#
+
+messy_array = [1, 7, 3, 5, 2, 8, 9, 4, -6]
+#=> [-6, 1, 2, 3, 4, 5, 7, 8, 9]
+#p messy_array.sort
+#built-in sorting method
+
+def insertion_sort(array)
+  #method to build a new array of integers ruby print
+  sorted_array = [array[0]]
+  #new array with initial integer, item in position 0 from original array
+  array.delete_at(0)
+  #removes item in position 0 from original array
+  for i in array
+    #insertion sort method
+    #code evaluates value of initial integer with current integer, inserts ascending order
+    insert_index = 0
+    #index for looping
+    while insert_index < sorted_array.length
+      #loop continues for length of array
+      if i <= sorted_array[insert_index]
+        #evaluates for smaller numbers
+        sorted_array.insert(insert_index, i)
+        #if number is smaller than the initial integer, it places in front of array
+        break
+        #ends if code
+      elsif insert_index == sorted_array.length - 1
+        sorted_array.insert(insert_index+1, i)
+        #inserts larger or equal numbers, behind the initial integer in ascending order
+        break
+        #ends elsif code
+      end
+      #ends if loop
+      insert_index += 1
+    end
+    #ends while loop
+  end
+  #ends for loop
+  sorted_array
+  #prints new array organized in ascending order
+end
+#ends new method
+
+print insertion_sort(messy_array)
+# => [-6, 1, 2, 3, 4, 5, 7, 8, 9]
