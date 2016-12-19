1e3aecccab0c0a5fb7c240a9573a502245d1bdcd
matth3wjones <matthewjones00@me.com>
2016-12-18 22:45:52 -0800

    Update with Release 0

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -11,62 +11,52 @@
 #method replace vowel with looping array of vowels
 #method replace consonant with looping array of consonants
 
-#
-#
-#USER INTERFACE
-#
-
-puts "What is your first name?"
-#request for users first name
-first_name = gets.chomp.chars
-#save user input, separate first name by letter
-puts "What is your last name?"
-#request for users last name
-last_name = gets.chomp.chars
-#save user input, separate last name by letter
-
-p first_name
-#test
-p last_name
-#test
 
 #
 #
 #BUSINESS LOGIC
 #
 
-spy_name = []
-#empty array to hold name
-vowels = ["a", "e", "i", "o" "u"]
-#array of vowels
-consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
-#array of consonants
 
 
-def vowel_or_consonant(name_array)
+def vowel_or_consonant
+  puts "What is your first name?"
+  #request first name
+  first_name = gets.chomp.downcase
+  #save user input, remove capitalization
+  puts "What is your last name?"
+  #request for users last name
+  last_name = gets.chomp.downcase
+  #save user input, remove capitalization
   #method to check for letter type
-  index = 0
-  #loop index
-  vowels = ["a", "e", "i", "o" "u"]
-  #vowel array
-  consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
-  #consonant array
-  while index < name_array.length
-    #loop thru each character
-    if vowels.select! { || }
-      #check if vowel
-      puts name_array[index]
-      #test
-    end
-    #ends if loop
-    index += 1
-    #loop counter
-  end
-  #ends while loop
+  vowels_first = first_name.gsub(/[aeiou]/,'a'=>'e','e'=>'i','i'=>'o','o'=>'u','u'=>'a')
+  #check first name for vowels, replaces with next vowel
+  vowels_last = last_name.gsub(/[aeiou]/,'a'=>'e','e'=>'i','i'=>'o','o'=>'u','u'=>'a')
+  consonant_first = vowels_first.gsub(/[bcdfghjklmnpqrstvwxyz]/, 'b'=>'c', 'c'=>'d', 'd'=>'f', 'f'=>'g', 'g'=>'h', 'h'=>'j', 'j'=> 'k', 'k'=>'l', 'l'=>'m', 'm'=>'n', 'n'=>'p', 'p'=>'q', 'q'=>'r', 'r'=>'s', 's'=>'t', 't'=>'v', 'v'=>'w', 'w'=>'x', 'x'=>'y', 'y'=>'z', 'z'=>'b')
+  consonant_last = vowels_last.gsub(/[bcdfghjklmnpqrstvwxyz]/, 'b'=>'c', 'c'=>'d', 'd'=>'f', 'f'=>'g', 'g'=>'h', 'h'=>'j', 'j'=> 'k', 'k'=>'l', 'l'=>'m', 'm'=>'n', 'n'=>'p', 'p'=>'q', 'q'=>'r', 'r'=>'s', 's'=>'t', 't'=>'v', 'v'=>'w', 'w'=>'x', 'x'=>'y', 'y'=>'z', 'z'=>'b')
+  spy_name = "#{consonant_last.capitalize} #{consonant_first.capitalize}"
+  #declares spy name
+  puts "The name #{first_name.capitalize} #{last_name.capitalize} turns into #{spy_name}"
+  #displays product of method
 end
 #end vowel or not method
 
+#
+#
+#USER INTERFACE
+#
+#p first_name
+#test
+#p last_name
+#test
 
-
-puts vowel_or_consonant(first_name)
-puts vowel_or_consonant(last_name)
+input = nil
+while input != "quit"
+  puts "To generate spy name, type enter or quit"
+  input = gets.chomp.downcase
+  if input == "quit"
+    puts "Exit"
+  else
+    vowel_or_consonant
+  end
+end
