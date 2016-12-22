3e129528620b51569feec04282c226965e76410d
matth3wjones <matthewjones00@me.com>
2016-12-22 11:21:50 -0800

    update release 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -46,7 +46,7 @@ class Puppy
   end
 
 end
-#end class
+#end dog class
 
 fido = Puppy.new
 fido.fetch("ball")
@@ -54,3 +54,56 @@ fido.speak(3)
 fido.roll_over
 fido.dog_years(4)
 fido.sit
+
+#release 2
+class Kitten
+  #kitten class
+  def initialize
+    puts "Initializing new kitten instance ..."
+  end
+  #end initialize method
+
+  def meow
+    #meow method
+    puts "*meooooowwwws*"
+  end
+  #end meow meow
+
+  def cat_years(num)
+    #cat years
+    puts num*6
+  end
+  #ends cat years method
+
+  def sleep
+    #sleeps
+    puts "*sleeps*"
+  end
+  #end sleep method
+
+end
+#ends cat class
+
+index = 0
+#loop index
+kitten_array = []
+#empty array for storage
+while index < 50
+  #while loop for 50 instances
+  kitten_array << Kitten.new
+  #shovel new unnamed kittens into array
+  #puts index
+  #test
+  index += 1
+  #count up
+end
+#end while loop
+
+kitten_array.each do |cat|
+  #loop for iterating
+  cat.meow
+  #action to iterate
+  cat.cat_years(2)
+  #action to iterate
+end
+#ends do loop
