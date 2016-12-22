69f2bb3f8be56594d6dd335f79b3d43f0e1658c4
matth3wjones <matthewjones00@me.com>
2016-12-21 21:03:02 -0800

    update with puppy attributes in pseudocode

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+#SPECIES ------------------------
+#Greatis Deutsche Labradoris
+#
+#CHARACTERISTICS ----------------
+#Leg length: varies, 14-17 inches
+#Friendly: true
+#Coat color: black
+#Name: varies
+#Ears: natural
+#
+#BEHAVIOR -----------------------
+#Kiss/Lick
+#Run
+#Jump
+#Bite
