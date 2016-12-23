ba2d9d20f423875c78d917204fcdc1587661f445
matth3wjones <matthewjones00@me.com>
2016-12-22 18:00:09 -0800

    update with release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -27,61 +27,69 @@ class Santa
   def name
     #name getter method
     @name
-    #code
   end
   #ends name getter method
 
   def name=(new_name)
     #name setter method
     @name = new_name
-    #code
   end
   #ends name setter method
 
   def celebrate_birthday
-    #celebrate_birthday getter method
-    #TEST ME
-    @age += 1
-    #code
+    #age getter method
+    @age
   end
-  #ends celebrate_birthday getter method
+  #ends age getter method
 
-  def get_mad_at(reindeer_name)
+  def celebrate_birthday=(num)
+    #celebrate_birthday setter method
+    @age += num
+  end
+  #ends celebrate_birthday setter method
+
+  def get_mad_at
     #get_mad_at getter method
-    #TEST ME
-    @name
-    #code
+    @reindeer_ranking
   end
   #ends get_mad_at getter method
 
+  def get_mad_at=(reindeer_name)
+    #get_mad_at setter method
+    index = @reindeer_ranking.index(reindeer_name)
+    #identify the index of the string in the array
+    @reindeer_ranking.insert(-1, @reindeer_ranking.delete_at(index))
+    #delete the item from the array while re-adding at end of array
+  end
+  #ends get_mad_at setter method
+
   def gender
     #gender getter method
-    #TEST ME
     @gender
-    #code
   end
   #ends gender getter method
 
   def gender=(new_gender)
     #gender setter method
-    #TEST ME
     @gender = new_gender
-    #code
   end
   #ends gender setter method
 
   def age
     #age getter method
     @age
-    #
   end
   #ends age getter method
 
+  def ethnicity
+    #ethnicity getter method
+    @ethnicity
+  end
+  #neds ethnicity getter method
 
   def speak
     #speak method
     puts "Ho, ho, ho! Haaaappy holidays!"
-    #code
   end
   #ends speak method
 
@@ -97,20 +105,25 @@ end
 santa = Santa.new("Jeff", "male", "Caucasian")
 santa.speak
 santa.eat_milk_and_cookies("snickerdoodle")
-
-santas = []
-#empty array
-names = ["Kris", "Adrian", "Michel", "Pat", "Max", "Taylor", "Jamie", "Jordan"]
-#array of names
-genders = ["agender", "bigender", "female", "gender fluid", "intersex", "male", "other", "transgender"]
-#array of genders
-ethnicities = ["Arabs", "African-American", "Irish", "Jews", "Native American", "Spaniards", "Swedes", "Thais", ""]
-#array of ethnicities
-genders.length.times do |i|
-  #loop for number of items in gender array
-  santas << Santa.new(names[i], genders[i], ethnicities[i])
-  #add new santas to the array with variables from two arrays
-end
-#ends do loop
-
-
+santa.name = "Jeffrey"
+#setter test
+puts "#{santa.name}"
+#getter test
+santa.celebrate_birthday=(1)
+#setter test
+puts "#{santa.name} is #{santa.celebrate_birthday}"
+#getter test
+santa.celebrate_birthday=(1)
+puts "#{santa.name} is #{santa.celebrate_birthday}"
+#getter/setter test 2
+santa.celebrate_birthday=(1)
+puts "#{santa.name} is #{santa.celebrate_birthday}"
+#getter/setter test 3
+santa.gender=("other")
+#setter test
+puts "#{santa.name} has a gender of #{santa.gender}"
+#setter test
+santa.get_mad_at=("Vixen")
+#setter test
+puts "Santa's Reindeer Ranking #{santa.get_mad_at}"
+#getter test
