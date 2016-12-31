736d8a4dfd2c88608f40fe7ee66d5263875cf8e6
matth3wjones <matthewjones00@me.com>
2016-12-30 21:06:32 -0800

    update with 5 passing tests

--- ruby/guess_again/guess_again.rb
+++ ruby/guess_again/guess_again.rb
@@ -14,27 +14,50 @@ class Guessagain
 
   def initialize
     @secret_word = ""
+    @master_array = []
     @guess_count = 0
     @is_right = false
   end
 
-  def seperate_word(the_word)
-    the_word.split("")
+  def acceptr(secret_word)
+    @secret_word = secret_word
   end
 
-  def check_a_letter(letter, array)
+  def splitr(the_word)
+    @master_array = the_word.split("")
+  end
+
+  def checkr(letter, array)
     array.include?(letter)
   end
 
-  def hide_array(array)
-
+  def hider(array)
+    hidden_array = []
+    @master_array = hidden_array
+    index = 0
+    while index < array.length
+      @master_array << "_"
+      index += 1
+    end
+    @master_array
   end
 
-  # if complete win, else taunt
-  #
-  #
-  def win_or_taunt
-
+  def displayr(guess, array)
+    index = 0
+    working_array = []
+    @master_array = working_array
+    array.each do
+      if array[index] == guess
+        working_array << guess
+        index += 1
+      elsif @master_array[index] == nil
+        working_array << "_"
+        index += 1
+      else
+        index += 1
+      end
+    end
+    @master_array
   end
 
 end
@@ -44,13 +67,15 @@ end
 #
 #
 
-puts "."
-puts ".."
-puts "..."
-puts "Welcome to Guess Again... it's just like Hangman but no one gets hurt."
-puts "A player picks a secret word for another player to guess, one letter at a time."
-puts "Limit of 1 guess per letter in the secret word, so guess carefully"
-game = Guessagain.new
-guess_count = 0
+puts "What is your name?"
+usr1 = gets.chomp.capitalize!
+puts "Hi #{usr1}, you'll be picking a secret word, and having a friend guess, can you tell me who that will be?"
+usr2 = gets.chomp.capitalize!
+#puts "..."
+#puts "Welcome to Guess Again... it's just like Hangman but no #one #gets hurt."
+#puts "A player picks a secret word for another player to guess, #one #letter at a time."
+#puts "Limit of 1 guess per letter in the secret word, so guess #carefully"
+#game = Guessagain.new
+#guess_count = 0
 #intro of game
 
--- ruby/guess_again/guess_again_rspec.rb
+++ ruby/guess_again/guess_again_rspec.rb
@@ -3,28 +3,29 @@ require_relative 'guess_again'
 describe Guessagain do
   let(:game) { Guessagain.new}
 
-  it "separates word into array" do
-    expect(game.seperate_word("dog")).to eq ["d", "o", "g"]
+  it "accepts secret word" do
+    expect(game.acceptr("dog")).to eq "dog"
+  end
+
+  it "splits word into array" do
+    expect(game.splitr("dog")).to eq ["d", "o", "g"]
   end
   #ends method to separate word into array of letters
 
   it "checks if guess is present" do
-    expect(game.check_a_letter("d", ["d", "o", "g"])).to eq true
+    expect(game.checkr("d", ["d", "o", "g"])).to eq true
   end
   #end method to accept guess letter, scan array to see if present
 
   it "hide the real array" do
-    expect(game.hide_array(["d", "o", "g"])).to eq "_ _ _"
+    expect(game.hider(["d", "o", "g"])).to eq ["_", "_", "_"]
   end
   #ends hide the real array
 
   it "displays correct guesses in array" do
+    expect(game.displayr("d", ["d", "o", "g"])).to eq ["d", "_", "_"]
   end
   #ends method to display correct guesses in array
-
-  it "if complete win, else taunt" do
-  end
-  #ends win or taunt do loop
 
 end
 #ends describe do loop
