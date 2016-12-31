9bc60d119cc58cd1f47afdcbb4208aded1ee7e72
matth3wjones <matthewjones00@me.com>
2016-12-30 21:21:25 -0800

    update with intro driver code

--- ruby/guess_again/guess_again.rb
+++ ruby/guess_again/guess_again.rb
@@ -66,16 +66,25 @@ end
 # USER INTERFACE
 #
 #
-
+puts "WELCOME to".center(40)
+puts "Guess Again".center(40)
+puts "......".center(40)
+puts "......".center(35)
+puts ".....".center(30)
+puts "....".center(25)
+puts "...".center(20)
+puts "..".center(15)
+puts ".".center(10)
 puts "What is your name?"
-usr1 = gets.chomp.capitalize!
+usr1 = gets.chomp
 puts "Hi #{usr1}, you'll be picking a secret word, and having a friend guess, can you tell me who that will be?"
-usr2 = gets.chomp.capitalize!
-#puts "..."
-#puts "Welcome to Guess Again... it's just like Hangman but no #one #gets hurt."
-#puts "A player picks a secret word for another player to guess, #one #letter at a time."
-#puts "Limit of 1 guess per letter in the secret word, so guess #carefully"
-#game = Guessagain.new
-#guess_count = 0
+usr2 = gets.chomp
+puts "Ok #{usr2} and #{usr1}, let's get started.  Remember, #{usr2} will get one guess for each letter in the word provided by #{usr1}.  #{usr1} please enter the secret word"
+
+game = Guessagain.new
+guess_count = 0
 #intro of game
+
+
+
 
