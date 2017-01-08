741016c2f7c670e71499a7cd9f4bbb2784deaca8
matth3wjones <matthewjones00@me.com>
2017-01-08 13:07:14 -0800

    add reverse method

--- js/test.js
+++ js/test.js
@@ -1,1 +1,12 @@
 console.log("Hello world!");
+
+var str = "Loop over me!";
+
+// Look at line 8:
+// We initialize our counter, set its "while" condition,
+// and give the increment command that should happen
+// after each iteration of the loop --
+// all in one tight line of code!
+for (var i = 10; i > 0; i--) {
+  console.log(i);
+}
