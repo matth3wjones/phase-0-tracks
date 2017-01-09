cc82d737b21ed1c0e5412f7e90087b7010e8a7fd
matth3wjones <matthewjones00@me.com>
2017-01-08 16:32:28 -0800

    release 2 complete

--- js/algos.js
+++ js/algos.js
@@ -8,9 +8,9 @@
 //
 //
 
-var phrases = ["long phrase","longest phrase","longer phrase"]
+var phrases = ["long phrase","longest phrase","longer phrase"];
 
-function Longest(array) {
+function longest(array) {
   this.array = array;
   this.longest = "";
   for (var i = 0; i < array.length; i++) {
@@ -18,22 +18,35 @@ function Longest(array) {
       longest = array[i]
     };
   };
-  return longest
-}
+  return longest;
+};
 
 // write function takes 2 objects, checks common k/v pair
 //
 // compare two objects, return like pairs
 //
 
-var dude = {name: "Steven", age: 54}
-var girl = {name: "Tamir", age: 54}
+//var each = function(array, func) {
+//  for (var i = 0; i < array.length; i++) {
+//    func(array[i]);
+//  };
+//};
 
-function Match(obj1, obj2) {
-
+function match(obj1,obj2) {
+  return Object.keys(obj1).reduce(function(key,value) {
+    if (obj1[value] === obj2[value]) key[value] = obj2[value];
+    return key;
+    //returns matching keys
+  }, {});
 }
+
+var him = {name: "Steven", age: 54};
+var her = {name: "Tamir", age: 54}
+
 
 // DRIVER CODE
 //
 //
-console.log(Longest(phrases))
+console.log(longest(phrases));
+console.log(match(him, her));
+//each(dude, match(dude, girl))
