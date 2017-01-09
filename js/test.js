fa12053e9160b8ae827a1006263593a5790081b5
matth3wjones <matthewjones00@me.com>
2017-01-08 16:32:43 -0800

    test code

--- js/test.js
+++ js/test.js
@@ -10,3 +10,20 @@ var str = "Loop over me!";
 for (var i = 10; i > 0; i--) {
   console.log(i);
 }
+
+
+var select = function(array, func) {
+  var results = [];
+
+  for (var i=0; i < array.length; i++) {
+    if (func(array[i])) {
+      results.push(array[i]);
+    }
+  }
+
+  return results;
+};
+// Select the schools with names that have an 'r' in them:
+var schools = ["Berkeley", "Notre Dame", "Chicago", "Stanford", "Tulane"];
+
+select(schools, 'r')
