fc220a86fc340b1452423c4ef78d22ed966fa591
matth3wjones <matthewjones00@me.com>
2017-01-16 14:37:00 -0800

    working puppy maker

--- databases/puppy_maker/puppy_maker.rb
+++ databases/puppy_maker/puppy_maker.rb
@@ -1,5 +1,29 @@
 require 'sqlite3'
 require 'faker'
 
-database = SQLite3::Database.new("puppies.db")
+db = SQLite3::Database.new("puppies.db")
 
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS puppies(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    age INT
+  )
+SQL
+
+db.execute(create_table_cmd)
+db.results_as_hash = true
+#db.execute("INSERT INTO puppies (name, age) VALUES ('Gunner', 1)")
+
+#puppies = db.execute("SELECT * FROM puppies")
+#puppies.each do |puppy|
+ # puts "#{puppy['name']} is #{puppy['age']}"
+#end
+
+def create_puppy(db, name, age)
+  db.execute("INSERT INTO puppies (name, age) VALUES (?, ?)", [name, age])
+end
+
+10.times do
+  create_puppy(db, Faker::Name.name, 0)
+end
