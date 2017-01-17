edf1c1048882909de8c00396826814242761ec8e
matth3wjones <matthewjones00@me.com>
2017-01-16 20:32:57 -0800

    add working loop to request data

--- databases/persistent/persistent.rb
+++ databases/persistent/persistent.rb
@@ -1,14 +1,12 @@
 require 'sqlite3'
-require 'faker'
 
 class Itinerary
-
   def initialize
     @db = SQLite3::Database.new("list.db")
     @db.results_as_hash = true
 
     create_table = <<-SQL
-      CREATE TABLE IF NOT EXISTS list(
+      CREATE TABLE IF NOT EXISTS list (
         id INTEGER PRIMARY KEY,
         dest VARCHAR(255),
         date VARCHAR(255),
@@ -18,18 +16,56 @@ class Itinerary
     @db.execute(create_table)
   end
 
-  def create(db, dest, date, days)
-    puts "_" * 70
-    puts "\n\n"
-    puts "WELCOME"
+  def save(dest, date, days)
+    @db.execute("INSERT INTO list (dest, date, days) VALUES (?, ?, ?)", [dest, date, days])
   end
 
-  def to_s
-    "Hi"
+  def run
+    puts "Type GO if you want to get away, SEE to view your itinerary or EXIT"
+    input = gets.chomp.upcase
+    case input
+    when 'GO' then go
+    when 'SEE' then see
+    when 'EXIT' then exit
+    else puts "Please type GO, SEE or EXIT:"
+      input = gets.chomp.upcase
+    end
+  end
+
+  def see
+    list = @db.execute("SELECT * from list")
+    list.each do |db|
+      puts "#{db['dest']} for #{db['days']} nights starting #{db['date']}"
+    end
+    run
+  end
+
+  def go
+    puts "Where are you going?"
+    dest = gets.chomp
+    puts "When do you arrive?"
+    date = gets.chomp
+    puts "How many nights in #{dest}?"
+    days = gets.chomp
+    save(dest, date, days)
+    run
   end
 
 end
 
 itin = Itinerary.new
 # puts itin
-itin.create(itin, "Santorini", "8/22/2017", 2)
+#input = false
+#while != true
+#  puts "Where are you going?"
+#  dest = gets.chomp
+#  puts "When do you arrive?"
+#  date = gets.chomp
+#  puts "How many nights in #{dest}?"
+#  days = gets.chomp
+#
+#  itin.save(dest, date, days)
+#end
+
+
+itin.run
